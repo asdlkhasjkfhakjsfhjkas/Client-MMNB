d435a20032de7c7cf75e1f840198332780eefdb4
myhonestface <137356024+vermuzui@users.noreply.github.com>
2023-06-26 00:23:57 +0200

    v1.0.0

---
+++ src/assets/js/panels/panelSkin.js
@@ -0,0 +1,19 @@
+/**
+ * @author 2K Studio
+ * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0/
+ */
+
+'use strict';
+
+import { database } from '../utils.js';
+
+
+class Skin {
+    static id = "panelSkin";
+    async init(config) {
+        this.config = config
+        this.database = await new database().init();
+    }
+}
+
+export default Skin;
